a1aa3f831010fabaaa37e4c87b92f5b660583643
WDireX <gsramazan12@gmail.com>
2020-08-20 18:53:47 +0300

    Added logo

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var http = require("http").createServer(app);
 var path = require("path");
 
 app.use(express.static(path.join(__dirname,/*"..",*/"dist")));
-//app.use(express.static(path.join(__dirname,/*"..",*/"media")));
+app.use(express.static(path.join(__dirname,"media")));
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, /*"..",*/ "dist", "index.html"));
 });
 
-/*app.get("/media/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "media", req.params[0]));
-});*/
+app.get("/media/*", function (req, res) {
+  res.sendFile(path.join(__dirname,"media", req.params[0]));
+});
 
 http.listen(process.env.PORT || 3000, () => { console.log("[SERVER]: I am started") });
